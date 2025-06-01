d0a5177696b14308d99eb478c217225af0c9e1a1
Diego Aguilera <degojk@gmail.com>
2025-05-31 22:02:15 -0400

    cambios del joke

---
+++ Tailwind.config.mjs
@@ -0,0 +1,9 @@
+// tailwind.config.mjs
+export default {
+    darkMode: 'class',
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
