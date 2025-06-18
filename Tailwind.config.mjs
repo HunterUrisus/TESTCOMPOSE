5e9983ffdd56c09ac5af5ba2ff35dac080dd2661
Joke <degojk@gmail.com>
2025-06-18 00:54:24 -0400

    Actualizacion con imagenes para la pagina principal

--- Tailwind.config.mjs
+++ Tailwind.config.mjs
@@ -3,7 +3,19 @@ export default {
     darkMode: 'class',
     content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                    fadeIn: {
+                    '0%': { opacity: '0', transform: 'translateY(20px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                    },
+                },
+                animation: {
+                    fadeIn: 'fadeIn 0.8s ease-out forwards',
+                },
+        },
     },
     plugins: [],
 }
+
+
